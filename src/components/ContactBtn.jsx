79f260f23e1360885a2ebb1bec904695a5ee881c
soufyane khalfallah <khalfalasoufi@gmail.com>
2023-12-28 16:37:35 +0300

    button to a

--- src/components/ContactBtn.jsx
+++ src/components/ContactBtn.jsx
@@ -1,14 +1,14 @@
 import { whatsApp_stikey } from "../assets";
 
 const ContactBtn = () => (
-  <button className=" whatsapp  w-10 h-10 rounded-full ">
+  <a className=" whatsapp  w-10 h-10 rounded-full ">
     <img
       src={whatsApp_stikey}
       alt="whatsApp_stikey"
       className="w-full
     "
     />
-  </button>
+  </a>
 );
 
 export default ContactBtn;
